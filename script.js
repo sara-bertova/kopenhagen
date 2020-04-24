8d753f9d923d316759f11c0e10760782e7df1197
sara-bertova <57212661+sara-bertova@users.noreply.github.com>
2020-04-24 09:15:33 +0200

    nav menu - attempt

---
+++ script.js
@@ -0,0 +1,21 @@
+fetch("http://rasbery.eu/kph/wp-json/wp/v2/categories")
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        handleData(data)
+    })
+
+function handleData(jsonData) {
+    jsonData.forEach(showNav)
+
+}
+
+function showNav(nav) {
+    console.log(nav.name)
+
+    const cat = document.createElement("a");
+    cat.textContent = `${nav.name}` + " / ";
+
+    document.querySelector(".navm").appendChild(cat);
+}

199e02fcdfdf69eab898282069be44b237d5afa5
zuzanatrembulakova <zuzanatrembulakova@users.noreply.github.com>
2020-04-24 18:58:22 +0200

    back to top button added

--- script.js
+++ script.js
@@ -31,3 +31,28 @@ const cursor = document.querySelector(".cursor")
 document.addEventListener("mousemove", e => {
     cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
 })
+
+/*-------GO TO TOP BTN------------------------------*/
+
+//enable/disable scroll button based on scroller position
+function scrollFunction() {
+    var topBtn = document.getElementById("topBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+}
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+
+/*---GO TO TOP BTN-------------------------------------*/
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() { // eslint-disable-line no-unused-vars
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
